3a14f4f1fc0a73b23860fa6eb9b5138684829e69
zohair52 <52715262+zohair52@users.noreply.github.com>
2019-12-05 15:18:53 -0800

    Add files via upload

--- Suffix_Contruct.java
+++ Suffix_Contruct.java
@@ -6,21 +6,21 @@ import java.util.Map;
 
 
 public class Suffix_Contruct {
-	static class TrieNode {
-		Map<Character, TrieNode> children = new HashMap<Character, TrieNode>();
+	static class TreeNode {
+		Map<Character, TreeNode> children = new HashMap<Character, TreeNode>();
 		
 	}
 	static class SuffixTrie {
 		
-		TrieNode root = new TrieNode();
-		char endSymbol = '*';
+		TreeNode root = new TreeNode();
+		
 		
 		public SuffixTrie(String str) {
 			populateStuffixTrieFrom(str);
 		}
 		public void populateStuffixTrieFrom(String str) {
 			
-			for (int i = 0; i<str.length(); i++) {
+			for (int i = 0; i < str.length(); i++) {
 				
 				insertSubstringStartingAt(i, str);
 			}
@@ -28,23 +28,23 @@ public class Suffix_Contruct {
 		
 		public void insertSubstringStartingAt (int i, String str) {
 			
-			TrieNode node = root;
+			TreeNode node = root;
 			for (int j = i; j < str.length(); j++) {
 				
 				char letter = str.charAt(j);
 				if (!node.children.containsKey(letter)) {
 					
-					TrieNode newNode = new TrieNode();
+					TreeNode newNode = new TreeNode();
 					node.children.put(letter, newNode);
 				}
 				node = node.children.get(letter);
 			}
-			node.children.put(endSymbol, null);
+			
 		}
 		
 		public boolean contrains(String str) {
 			
-			TrieNode node=  root;
+			TreeNode node=  root;
 			for (int i = 0; i < str.length(); i++) {
 				
 				char letter = str.charAt(i);
@@ -54,7 +54,7 @@ public class Suffix_Contruct {
 				node= node.children.get(letter);
 				
 			}
-			return node.children.containsKey(endSymbol) ? true : false;
+			return true;
 		}
 	}
 }
